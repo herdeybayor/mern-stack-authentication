00d250196921ca938d83ca3ef5d02e2442db722f
Sherifdeen <shereefdeen4real@gmail.com>
2022-01-12 00:28:14 +0100

    Create Profile.js

---
+++ client/src/components/Profile.js
@@ -0,0 +1,9 @@
+function Profile() {
+  return (
+    <div className="flex h-screen justify-center items-center bg-teal-200 dark:bg-black">
+      <h1>User Profile</h1>
+    </div>
+  );
+}
+
+export default Profile;
